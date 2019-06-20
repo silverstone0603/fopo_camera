053b14ea73cf7b0a7320844c671a9b1965268e87
SONgiwon <49133412+SONgiwon@users.noreply.github.com>
2019-06-21 02:22:09 +0900

    modMemProcess : 회원가입처리 MainActivity : 지금 첫 앱 실행시 회원가입 UI Fragment실행시켜 놨음
    SignUpActivity : 회원가입버튼 클릭시 처리 엑티비티
    
    login, register xml파일 추가해놨음.

---
+++ app/src/main/java/com/teamfopo/fopo/module/modMemProcess.kt
@@ -0,0 +1,45 @@
+package com.teamfopo.fopo.module
+
+import android.os.AsyncTask
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import okhttp3.*
+
+class modMemProcess {
+
+    inner class signUp : AsyncTask<String, Long, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+            var mem_id = params[0]
+            var mem_pw = params[1]
+            var mem_nick = params[2]
+            var mem_gender = params[3]
+            var url = "http://106.10.51.32/ajax_process/member_process"
+            val requestBody: RequestBody = FormBody.Builder()
+                .add("type", "signUp")
+                .add("mem_id", "$mem_id")
+                .add("mem_pw", "$mem_pw")
+                .add("mem_nick", "$mem_nick")
+                .add("mem_gender", "$mem_gender")
+                .build()
+
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build()
+
+            val response: Response = client.newCall(request).execute()
+            var str = response.body()?.string()!!
+
+            return str
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+    }
+}
