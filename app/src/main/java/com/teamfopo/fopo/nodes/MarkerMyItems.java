c805f6db4e1f665958a9fa0a9f38703895eebd85
EunSeok Oh <srtay0561@hotmail.com>
2019-06-21 19:22:59 +0900

    We will rock you, once again.
    Signed-off-by: EunSeok Oh <srtay0561@hotmail.com>

---
+++ app/src/main/java/com/teamfopo/fopo/nodes/MarkerMyItems.java
@@ -0,0 +1,37 @@
+package com.teamfopo.fopo.nodes;
+
+mport com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class MarkerMyItems implements ClusterItem {
+    private final LatLng mPosition;
+    private String mTitle;
+    private String mSnippet;
+
+    public MarkerMyItems(double lat, double lng) {
+        mPosition = new LatLng(lat, lng);
+    }
+    public MarkerMyItems(double lat, double lng, String title, String snippet) {
+        mPosition = new LatLng(lat, lng);
+        mTitle=title;
+        mSnippet = snippet;
+    }
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+
+    @Override
+    public String getTitle() {
+        return mTitle;
+    }
+
+    @Override
+    public String getSnippet() {
+        return mSnippet;
+    }
+
+}
